421af99ddb433553ccebee1c4bb11d95a83dac63
dzhem911 <dzhemal911@gmail.com>
2022-07-25 21:07:11 +0300

    package.jsonq & new routess

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import App from './App';
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
