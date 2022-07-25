4ec6e74098277d0cde4dbe2f50834ddef66eadc9
dzhem911 <dzhemal911@gmail.com>
2022-07-25 16:10:44 +0300

    new api path

--- src/https/index.js
+++ src/https/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://avista-analytic.herokuapp.com/api'
+export const API_URL = 'https://analytic-avista.herokuapp.com/'
 // export const API_URL = 'http://localhost:5001/api'
 
 const $api = axios.create({
