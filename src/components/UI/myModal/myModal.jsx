ff3cd264d5a41da3d0791b8a92d2f0bf6ad4a6a5
dzhem911 <dzhemal911@gmail.com>
2022-08-20 13:18:09 +0300

    present first step2

--- src/components/UI/myModal/myModal.jsx
+++ src/components/UI/myModal/myModal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import modalStyle from './modal.module.css'
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const MyModal = ({children, visible, setVisible, setStep, setSignInVisible, signInVisible, step, pasResetModal, setPasResetModal}) => {
 
@@ -25,6 +26,7 @@ const MyModal = ({children, visible, setVisible, setStep, setSignInVisible, sign
 
   return (
     <div className={rootClasses.join(' ')} onClick={clickHandler}>
+      <CancelIcon className={modalStyle.closeIcon} />
       <div className={modalStyle.myModalContent} onClick={e => e.stopPropagation()}>
         {children}
       </div>
