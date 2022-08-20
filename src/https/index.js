5ddd563d1f8759647894dc91f039d878141645f1
dzhem911 <dzhemal911@gmail.com>
2022-08-20 13:20:10 +0300

    present first step3

--- src/https/index.js
+++ src/https/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-export const API_URL = 'http://localhost:5001/api'
-// export const API_URL = 'https://analytic-avista.herokuapp.com/api'
+// export const API_URL = 'http://localhost:5001/api'
+export const API_URL = 'https://analytic-avista.herokuapp.com/api'
 
 const $api = axios.create({
   withCredentials: true,
